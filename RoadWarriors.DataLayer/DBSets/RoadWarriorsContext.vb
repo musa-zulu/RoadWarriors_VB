a8bad6d91493c3150d00de057b049a8ef2281a3a
Celumusa Zulu <celumusa@bbd.co.za>
2016-08-11 08:40:20 +0200

    added RoadWarriorsContext

---
+++ RoadWarriors.DataLayer/DBSets/RoadWarriorsContext.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.Entity
+
+Public Class RoadWarriorsContext
+    Inherits DbContext
+
+    Public Property Athletes() As DbSet(Of Athlete)
+    Public Property AthleticEvents() As DbSet(Of AthleticEvent)
+    Public Property RaceResults() As DbSet(Of RaceResults)
+
+    Protected Overrides Sub OnModelCreating(ByVal modelBuilder As DbModelBuilder)
+    End Sub
+End Class
